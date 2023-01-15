1d113a39a7046610bbaa2e7adc2e083a8da1e0e4
omchand <omchandjoker27@gmail.com>
2023-01-15 22:24:41 +0530

    backend deploy

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const cors = require("cors")
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+
+app.use(cors({
+  origin:"*"
+}))
 
 app.get("/", (req, res) => {
   res.send("Welcome to Home Page");
